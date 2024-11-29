fad1de2c44f547bae2cdadf1aee0bbc8cc00def7
Imanuel Alexander Here <i.alex.here2004@gmail.com>
2024-11-29 10:09:20 +0700

    init spring project

---
+++ src/main/java/com/rpl/project_sista/ProjectSistaApplication.java
@@ -0,0 +1,13 @@
+package com.rpl.project_sista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpl/project_sista/ProjectSistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpl.project_sista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
